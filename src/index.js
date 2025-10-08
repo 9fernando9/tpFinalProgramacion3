95f87229cde45ae16effcd3e89483fc5aaad501e
aflores14 <alesjuy@gmail.com>
2025-10-07 22:24:23 -0300

    mejores practicas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import helmet from 'helmet';
+import validateContentType from "./middlewares/validateContentType.js";
 import routes from './routes/index.routes.js';
 
 const app = express();
@@ -6,6 +8,9 @@ const app = express();
 app.use(express.json());
 // ROUTES
 app.use(routes);
+app.use(validateContentType);
+app.use(helmet());
+app.use(express.json({ type: 'application/json' })); // solo acepta JSON
 
 const PORT = process.env.PORT || 3000;
 
